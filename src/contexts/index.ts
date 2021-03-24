22b5cb1afc2637af8aee2770815e02ea99b45801
Brad Garropy <bradgarropy@gmail.com>
2021-03-24 11:22:07 -0500

    feat: create storefront instance context and include it with global contexts
    SEARCH-1160

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -5,4 +5,5 @@
 
 export * from "./search";
 export { default as createShopperCtx } from "./shopper";
+export { default as createStorefrontInstanceCtx } from "./storefrontInstance";
 export { default as createTrackerCtx } from "./tracker";
---
+++ src/contexts/storefrontInstance.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+import { Context } from "@adobe/magento-storefront-events-sdk/dist/types/types/contexts";
+import mse from "@adobe/magento-storefront-events-sdk";
+import schemas from "../schemas";
+
+const createContext = (): Context => {
+    const storefrontInstanceCtx = mse.context.getStorefrontInstance();
+
+    const context = {
+        schema: schemas.STOREFRONT_INSTANCE_SCHEMA_URL,
+        data: {
+            baseCurrencyCode: storefrontInstanceCtx.baseCurrencyCode,
+            environment: storefrontInstanceCtx.environment,
+            environmentId: storefrontInstanceCtx.environmentId,
+            instanceId: storefrontInstanceCtx.instanceId,
+            storeCode: storefrontInstanceCtx.storeCode,
+            storeId: storefrontInstanceCtx.storeId,
+            storeName: storefrontInstanceCtx.storeName,
+            storeUrl: storefrontInstanceCtx.storeUrl,
+            storeViewCode: storefrontInstanceCtx.storeViewCode,
+            storeViewCurrencyCode: storefrontInstanceCtx.storeViewCurrencyCode,
+            storeViewId: storefrontInstanceCtx.storeViewId,
+            storeViewName: storefrontInstanceCtx.storeViewName,
+            websiteCode: storefrontInstanceCtx.websiteCode,
+            websiteName: storefrontInstanceCtx.websiteName,
+            websiteId: storefrontInstanceCtx.websiteId,
+        },
+    };
+
+    return context;
+};
+
+export default createContext;
