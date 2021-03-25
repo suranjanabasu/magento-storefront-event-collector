0d0ffecb0a322a7ca458d4a5c3a90b9515cbf144
Brad Garropy <bradgarropy@gmail.com>
2021-03-25 11:19:47 -0500

    feat: create shopping cart context and include it with global contexts
    SEARCH-1164

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -5,6 +5,7 @@
 
 export * from "./search";
 export { default as createShopperCtx } from "./shopper";
+export { default as createShoppingCartCtx } from "./shoppingCart";
 export { default as createStorefrontInstanceCtx } from "./storefrontInstance";
 export { default as createTrackerCtx } from "./tracker";
 export { default as createMagentoExtensionCtx } from "./extension";
---
+++ src/contexts/shoppingCart.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+import { Context } from "@adobe/magento-storefront-events-sdk/dist/types/types/contexts";
+import mse from "@adobe/magento-storefront-events-sdk";
+import schemas from "../schemas";
+
+const createShoppingCartItems = () => {
+    const shoppingCartCtx = mse.context.getShoppingCart();
+
+    if (!shoppingCartCtx.items) {
+        return [];
+    }
+
+    const shoppingCartItems = shoppingCartCtx.items.map(item => ({
+        basePrice: item.product.pricing?.regularPrice,
+        // TODO: how do we reconcile string to int
+        cartItemId: parseInt(item.id) || 0,
+        mainImageUrl: item.product.mainImageUrl,
+        // TODO: what price should we use?
+        offerPrice: item.prices?.[0].value,
+        productName: item.product.name,
+        productSku: item.product.sku,
+        qty: item.quantity,
+    }));
+
+    return shoppingCartItems;
+};
+
+const createContext = (): Context => {
+    const shoppingCartCtx = mse.context.getShoppingCart();
+    const orderCtx = mse.context.getOrder();
+
+    const context = {
+        schema: schemas.SHOPPING_CART_SCHEMA_URL,
+        data: {
+            // TODO: how do we reconcile string to int
+            cartId:
+                shoppingCartCtx.id === null
+                    ? null
+                    : parseInt(shoppingCartCtx.id),
+            itemsCount: shoppingCartCtx.totalQuantity,
+            items: createShoppingCartItems(),
+            // TODO: where does this come from?
+            possibleOnepageCheckout: false,
+            // TODO: confirm this is correct
+            subtotalAmount: orderCtx.subtotalExcludingTax,
+            subtotalExcludingTax: orderCtx.subtotalExcludingTax,
+            subtotalIncludingTax: orderCtx.subtotalIncludingTax,
+            // TODO: where does these come from?
+            giftMessageSelected: false,
+            giftWrappingSelected: false,
+        },
+    };
+
+    return context;
+};
+
+export default createContext;
