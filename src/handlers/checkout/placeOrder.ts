f10c94b37f5cf939caadb702e5a6516eb65f6461
Brad Garropy <bradgarropy@gmail.com>
2021-05-20 15:35:40 -0500

    fix: include the shopping cart context with the place order event

--- src/handlers/checkout/placeOrder.ts
+++ src/handlers/checkout/placeOrder.ts
@@ -4,16 +4,30 @@
  */
 
 import { Event } from "@adobe/magento-storefront-events-sdk/dist/types/types/events";
-import { trackStructEvent } from "@snowplow/browser-tracker";
+import {
+    SelfDescribingJson,
+    trackStructEvent,
+} from "@snowplow/browser-tracker";
+
+import { createShoppingCartCtx } from "../../contexts";
 
 const handler = (event: Event): void => {
-    const { pageContext, orderContext } = event.eventInfo;
+    const { pageContext, orderContext, shoppingCartContext } = event.eventInfo;
+
+    const shoppingCartCtx = createShoppingCartCtx(shoppingCartContext);
+
+    const context: Array<SelfDescribingJson> = [];
+
+    if (shoppingCartCtx) {
+        context.push(shoppingCartCtx);
+    }
 
     trackStructEvent({
         category: "checkout",
         action: "place-order",
         label: orderContext?.orderId.toString(),
         property: pageContext?.pageType,
+        context,
     });
 };
 
