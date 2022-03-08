33fe3c16ef13538d0de81528aae0016884f0686d
Graham Rogers <grahamr@adobe.com>
2022-03-08 15:09:01 -0800

    Dint 162: 🐛  alloy being configured twice (#107)
    * alloy doesn't get configured twice
    
    * fix window error for alloy
    
    * fix error handling
    
    * making sure load order of sdk doesn't matter
    
    * remove try/catch and reject on invalid context
    
    * remove redundant resolve
    
    * readability

--- src/alloy.ts
+++ src/alloy.ts
@@ -1,45 +1,56 @@
 import { createInstance } from "@adobe/alloy";
 
-import { createAEPCtx } from "./contexts";
+import { AlloyInstance } from "./aep/types/alloy.types";
+import { isValidContext } from "./contexts/aep";
+import createContext from "./contexts/aep";
+import { BeaconSchema } from "./types/aep";
 import { AEPContext } from "./types/contexts";
 
-const alloy = createInstance({ name: "alloy" });
+/*
+    We create a wrapper around `@adobe/alloy` for better type protection.
+*/
 
-function isValid(aep: AEPContext) {
-    return (
-        aep.datastreamId &&
-        aep.datastreamId !== "" &&
-        aep.imsOrgId &&
-        aep.imsOrgId !== ""
-    );
-}
+const alloyInstance: AlloyInstance = createInstance({ name: "alloy" });
 
-function configureAlloy(): Promise<any> {
-    const aepCtx: AEPContext = createAEPCtx();
+/* configure alloy and assign it to the window object */
+const configure = async (): Promise<AlloyInstance> => {
+    const aepCtx: AEPContext = createContext();
 
-    return new Promise((resolve, reject) => {
-        if (isValid(aepCtx)) {
-            alloy("configure", {
-                edgeConfigId: aepCtx.datastreamId,
-                orgId: aepCtx.imsOrgId,
-                // TODO ahammond: remove debug when feature complete
-                debugEnabled: true,
-            }).then(() => {
-                resolve(alloy);
-            });
-        } else {
-            reject();
-        }
-    });
-}
+    if (isValidContext(aepCtx)) {
+        await alloyInstance("configure", {
+            edgeConfigId: aepCtx.datastreamId as string,
+            orgId: aepCtx.imsOrgId as string,
+            // TODO ahammond: possibly remove debug when feature complete
+            debugEnabled: true,
+        });
 
-const getAlloy = (): Promise<any> => {
-    return window.alloy
-        ? Promise.resolve(window.alloy)
-        : configureAlloy().then(alloy => {
-              window.alloy = alloy;
-              return alloy;
-          });
+        window.alloy = alloyInstance;
+
+        return window.alloy;
+    } else {
+        return Promise.reject();
+    }
 };
 
-export { getAlloy };
+/** return alloy instance if it exists on the window, configures instance if it doesn't exist */
+export const getAlloy = async (): Promise<AlloyInstance> => {
+    if (typeof window.alloy === "function") {
+        return window.alloy;
+    }
+
+    // if alloy doesn't exist on the window, configure it
+    const windowAlloy = await configure();
+
+    return windowAlloy;
+};
+
+/** sends event payload that matches the BeaconSchema that's been defined */
+const sendEvent = async (schema: BeaconSchema): Promise<void> => {
+    const instance = await getAlloy();
+    const xdm = { xdm: { ...schema } };
+
+    await instance("sendEvent", xdm);
+};
+
+/** preconfigured alloy instance that allows us to send an event */
+export const alloy = { sendEvent, configure };
