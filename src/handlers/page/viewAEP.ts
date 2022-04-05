42923a9052d89739dd0b9eb39db89f7c3bfe568e
Graham Rogers <grahamr@adobe.com>
2022-04-05 14:22:34 -0700

    mapping correct page name

--- src/handlers/page/viewAEP.ts
+++ src/handlers/page/viewAEP.ts
@@ -1,10 +1,25 @@
+/* eslint-disable no-console */
 import { Event } from "@adobe/magento-storefront-events-sdk/dist/types/types/events";
 
 import { sendEvent } from "../../alloy";
 import { BeaconSchema } from "../../types/aep";
 
+/**
+ * AEP Handler for page view event
+ */
 const aepHandler = async (event: Event): Promise<void> => {
     const { pageContext, debugContext } = event.eventInfo;
+
+    // if we have a name return name, else return type
+    const handlePageName = () => {
+        const { pageType: type, pageName: name } = pageContext;
+
+        if (!name || !type) {
+            return undefined;
+        }
+
+        return name ? name : type;
+    };
 
     const payload: BeaconSchema = {
         _id: debugContext?.eventId,
@@ -18,7 +33,7 @@ const aepHandler = async (event: Event): Promise<void> => {
                 siteSection: pageContext.pageType,
                 /** temporary until sdk update gets merged */
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                name: (pageContext as any).pageName || undefined,
+                name: handlePageName(),
             },
         },
     };
